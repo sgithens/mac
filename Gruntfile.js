2d4daa7209fca1c00fabd8a2971c19d2fa74810e
Steven Githens <swgithen@mtu.edu>
2014-04-15 22:49:48 -0400

    GPII-88 Moving all functionality for building universal to our plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,12 @@
 module.exports = function(grunt) {
-    var path = require("path");
-
-    var node_modules = ".."+path.sep+"node_modules";
-    var universal = ".."+path.sep+"node_modules"+path.sep+"universal";
-    var repoURL = "git://github.com/GPII/universal.git";
-    var startDir = process.cwd();
-    var shellOptions = {
-        stdout: true,
-        stderr: true
-    };
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
-        shell: {
-            gitClone: { 
-                options: shellOptions,
-                command: function() {
-                    return 'git clone --depth=1 ' + repoURL + " " + universal; 
-                }
-            },
-            npmInstall: {
-                options: {
-                    stdout: true,
-                    stderr: true,
-                    execOptions: {
-                        cwd: universal
-                    }
-                },
-                command: function() {
-                    return 'npm install';
-                }
-            },
-        }
+        // TODO show how to override node_modules, universal, and repoURL
     });
 
-    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks("grunt-gpii");
 
     grunt.registerTask('buildall', "Build the entire GPII", function() {
-        grunt.file.mkdir(node_modules);
-        grunt.task.run("shell:gitClone");
-        grunt.task.run("shell:npmInstall");
+        grunt.task.run("gpiiUniversal"); 
     }); 
 };
